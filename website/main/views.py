e2c02f301d503e67ba2029c8a2f7fc78b7b70aec
Your Name <Toas201513>
2022-10-16 22:38:32 +0300

    ban and user permission

--- website/main/views.py
+++ website/main/views.py
@@ -3,18 +3,36 @@ from.forms import PostForm, RegisterForm
 from django.contrib import messages
 from django.shortcuts import render,redirect
 from django.contrib.auth import login,logout,authenticate
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User,Group
+from django.contrib.auth.decorators import login_required,permission_required
 
 # Create your views here.
 @login_required(login_url="/login")
 def home(request):
-    posts=Post.objects.all()
-    
+    posts = Post.objects.all()
+
     if request.method == "POST":
         post_id = request.POST.get("post-id")
-        post=Post.objects.filter(id=post_id).first()
-        if post and post.author == request.user:
-            post.delete() 
+        user_id = request.POST.get("user-id")
+
+        if post_id:
+            post = Post.objects.filter(id=post_id).first()
+            if post and (post.author == request.user or request.user.has_perm("main.delete_post")):
+                post.delete()
+        elif user_id:
+            user = User.objects.filter(id=user_id).first()
+            if user and request.user.is_staff:
+                try:
+                    group = Group.objects.get(name='default')
+                    group.user_set.remove(user)
+                except:
+                    pass
+
+                try:
+                    group = Group.objects.get(name='ceo')
+                    group.user_set.remove(user)
+                except:
+                    pass
     return render(request,'main/home.html',{"posts":posts})
     
 
@@ -33,6 +51,7 @@ def sign_up(request):
     return render(request, 'registration/sign_up.html', {"form": form})
 
 @login_required(login_url="/login")
+@permission_required("main.add_post",raise_exception=True)
 def create_post(request):
     if request.method=='POST':
         form=PostForm(request.POST)
